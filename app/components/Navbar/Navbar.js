42b3bcafbdec5611e191a4322d08820e93cc9d9a
Himanshu-angularminds <Himanshu@angularminds.com>
2023-10-13 18:35:05 +0530

    small change

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -46,12 +46,12 @@ const Navbar = ({ currentUser }) => {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link" href="my-profile">
-                  Home
+                  Profile
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="/users">
-                  User
+                  User Data
                 </a>
               </li>
             </ul>
